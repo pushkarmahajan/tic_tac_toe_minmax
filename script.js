c6b59c79ec53f3bffed6a9e0f81448b62fa9aea6
pushkarmahajan <pushkarmahajan2202@gmail.com>
2020-11-23 16:43:12 +0530

    Deciding the winner

--- script.js
+++ script.js
@@ -30,4 +30,27 @@ function turnClick(square){
 function turn(squareId, player){
     orgboard[squareId] = player;
     document.getElementById(squareId).innerText=player;
+    let gameWon = checkWin(orgboard, player)
+    if (gameWon)gameOver(gameWon)
+};
+function checkWin(board,player){
+        let plays = board.reduce(( a, e, i) => (e===player) ? a.concat(i) : a,[]);
+        let gameWon = null;
+        for(let [index,win] of winCombination.entries()){
+            if(win.every(elem => plays.indexOf(elem) > -1)){
+                gameWon = {index: index, player: player};
+                break;
+            }
+        }
+        return gameWon;
 }
+function gameOver(gameWon){
+    for (let index of winCombination[gameWon.index]){
+        document.getElementById(index).style.backgroundColor = 
+        gameWon.player== huPlayer ? "blue" :  "red";
+    }
+    for(var i=0; i<blocks.length; i++){
+        blocks[i].removeEventListener('click',turnClick, false)
+    }
+}
+
